19476781d803c5828df8838ab993a1968e24cb26
bayaraa-1 <mongolasia724@gmail.com>
2023-07-29 17:22:00 +0800

    List MVC vvsgew

--- src/js/index.js
+++ src/js/index.js
@@ -3,6 +3,9 @@ import Search from "./model/Search";
 import { elements, renderLoader, clearLoader } from "./view/base";
 import * as searchView from "./view/searchView";
 import Recipe from "./model/Recipe";
+import List from "./model/List";
+import * as listView from "./view/listView";
+
 import {
   renderRecipe,
   clearRecipe,
@@ -67,25 +70,70 @@ const controlRecipe = async () => {
   // 1) URL-аас ID-ийг салгаж
   const id = window.location.hash.replace("#", "");
 
-  // 2) Жорын моделийг үүсгэж өгнө.
-  state.recipe = new Recipe(id);
+  // URL дээр ID байгаа эсэхийг шалгана
+  if (id) {
+    // 2) Жорын моделийг үүсгэж өгнө.
+    state.recipe = new Recipe(id);
 
-  // 3) UI дэлгэцийг бэлтгэнэ.
-  clearRecipe();
-  renderLoader(elements.recipeDiv);
-  highlightSelectedRecipe(id);
+    // 3) UI дэлгэцийг бэлтгэнэ.
+    clearRecipe();
+    renderLoader(elements.recipeDiv);
+    highlightSelectedRecipe(id);
 
-  // 4) Жороо татаж авчирна.
-  await state.recipe.getRecipe();
+    // 4) Жороо татаж авчирна.
+    await state.recipe.getRecipe();
 
-  // 5) Жорыг гүйцэтгэх хугацаа болон орцыг тооцоолно
-  clearLoader();
-  state.recipe.calcTime();
-  state.recipe.calcHuniiToo();
+    // 5) Жорыг гүйцэтгэх хугацаа болон орцыг тооцоолно
+    clearLoader();
+    state.recipe.calcTime();
+    state.recipe.calcHuniiToo();
 
-  // 6) Жороо дэлгэцэнд гаргана
-  renderRecipe(state.recipe);
+    // 6) Жороо дэлгэцэнд гаргана
+    renderRecipe(state.recipe);
+  }
 };
 
-window.addEventListener("hashchange", controlRecipe);
-window.addEventListener("load", controlRecipe);
+// window.addEventListener("hashchange", controlRecipe);
+// window.addEventListener("load", controlRecipe);
+
+["hashchange", "load"].forEach((e) =>
+  window.addEventListener(e, controlRecipe)
+);
+
+/**
+ * Найрлаганы контроллер
+ */
+
+const controlList = () => {
+  // Найрлаганы моделийг үүсгэнэ
+  state.list = new List();
+
+  // Өмнө харагдаж байсан найрлагануудыг дэлгэцээс зайлуулна.
+  listView.clearItems();
+
+  // Уг модел рүү одоо харагдаж байгаа жорны бүх найрлагыг авч хийнэ.
+  state.recipe.ingredients.forEach((n) => {
+    // Тухайн найрлагыг модел рүү хийнэ.
+    const item = state.list.addItem(n);
+
+    // Тухайн найрлагыг дэлгэцэнд гаргана.
+    listView.renderItem(item);
+  });
+};
+
+elements.recipeDiv.addEventListener("click", (e) => {
+  if (e.target.matches(".recipe__btn, .recipe__btn *")) {
+    controlList();
+  }
+});
+
+elements.shoppingList.addEventListener("click", (e) => {
+  // Клик хийсэн li элементийн data-itemid аттрибутыг шүүж гаргаж авах
+  const id = e.target.closest(".shopping__item").dataset.itemid;
+
+  // Олдсон ID-тэй орцыг моделоос устгана.
+  state.list.deleteItem(id);
+
+  // Дэлгэцээс ийм ID-тэй орцыг олж бас устгана.
+  listView.deleteItem(id);
+});
