702c8ff58a878c3b00220d5d40fc789bf70066b7
bayaraa-1 <mongolasia724@gmail.com>
2023-07-29 18:28:36 +0800

    last edit

--- webpack.config.js
+++ webpack.config.js
@@ -2,14 +2,11 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
   entry: "./src/js/index.js",
   output: {
     filename: "js/main.js",
     path: path.resolve(__dirname, "docs"),
-    // clean: true,
   },
-  devtool: "inline-source-map",
   devServer: {
     static: "./docs",
   },
